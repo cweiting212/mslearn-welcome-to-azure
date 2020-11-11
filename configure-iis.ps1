3bd215d52de0e80cf5c88af655d7140ad4b99403
cweiting212 <72126261+cweiting212@users.noreply.github.com>
2020-11-11 15:32:05 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -3,5 +3,5 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 
 # Set the home page.
 Set-Content `
-  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Path "mslearn-welcome-to-azure/Default.htm" `
   -Value "<html><body><h2>Welcome to Azure! My name is WeiTing Chen 20201111 $($env:computername).</h2></body></html>"
